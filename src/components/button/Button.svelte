ad4834951355e00928cd754bbf5dd2ae016470f7
pechischev <pechischev.vladimir@gmail.com>
2020-02-23 21:00:20 +0300

    feature(#3): заверстан блок Услуг
    добавлены стили для Button

--- src/components/button/Button.svelte
+++ src/components/button/Button.svelte
@@ -4,9 +4,43 @@
     const dispatch = createEventDispatcher();
 
     export let options = {};
-    const {text} = options;
+    export let variant = 'text';
+    export let disabled = false;
+
+    $: outlined = variant === 'outlined';
+    $: contained = variant === 'contained';
 </script>
 
-<button on:click={() => dispatch('click')}>
+<style lang="scss">
+.button {
+    font-size: 18px;
+    line-height: 24px;
+    text-align: center;
+    padding: 15px 30px;
+    border-radius: 29px;
+    border: 2px solid var(--white);
+    background: none;
+    outline: none;
+
+    &_outlined {
+        border: 2px solid var(--blue);
+        color: var(--blue);
+    }
+
+    &_contained {
+        border: 2px solid var(--blue);
+        background: var(--blue);
+        color: var(--white);
+    }
+}
+</style>
+
+<button
+    class="button"
+    class:button_contained={contained}
+    class:button_outlined={outlined}
+    class:button_disabled={disabled}
+    on:click={() => dispatch('click')}
+>
     <slot>{text}</slot>
 </button>
--- src/pages/landing/Services.svelte
+++ src/pages/landing/Services.svelte
@@ -9,24 +9,83 @@
     }
 </script>
 
+<style lang="scss">
+.services {
+    margin: 60px auto;
+}
+
+.service-groups {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 60px;
+
+    &__name {
+        margin: 0 5px;
+    }
+}
+
+.service {
+    display: flex;
+
+    &__image {
+        margin: 0 70px 0 70px;
+        width: 600px;
+    }
+
+    &__info {
+        padding-right: 60px;
+    }
+
+    &__title {
+        font-size: 36px;
+        color: var(--black);
+        line-height: 42px;
+        margin-bottom: 30px;
+    }
+
+    &__description {
+        font-size: 18px;
+        color: var(--bright-grey);
+        line-height: 27px;
+        margin-bottom: 30px;
+    }
+
+    &__items {
+        list-style-type: circle;
+        margin-left: 30px;
+        font-size: 21px;
+        color: var(--black);
+        line-height: 21px;
+    }
+}
+</style>
+
 <section class="services">
     <div class="service-groups">
         {#each services as service, index}
-            <Button on:click={() => selectService(index)}>{service.group}</Button>
+            <div class="service-groups__name">
+                <Button
+                    on:click={() => selectService(index)}
+                    variant={selectedService === service ? 'contained' : ''}
+                >{service.group}</Button>
+            </div>
         {/each}
     </div>
     <div class="services-container">
         <div class="service">
-            <div class="service__image">
-                <img alt="{selectedService.group}" title="{selectedService.group}" src="../assets/services/{selectedService.image}"/>
-            </div>
+            <img
+                class="service__image"
+                alt="{selectedService.group}"
+                title="{selectedService.group}"
+                src="../assets/services/{selectedService.image}"
+            />
             <div class="service__info">
                 <p class="service__title">{selectedService.title}</p>
                 <p class="service__description">{selectedService.description}</p>
                 <ul class="service__items">
-                {#each selectedService.items as item}
-                    <li>{item}</li>
-                {/each}
+                    {#each selectedService.items as item}
+                        <li style="margin: 0 0 20px">{item}</li>
+                    {/each}
                 </ul>
             </div>
         </div>
