9114f145a2e91e625d925dc511c2d7f4a12950fc
Vladimir Pechischev <pechischev.vladimir@gmail.com>
2020-02-28 20:28:41 +0300

    fix: поправлена сборка и устранен варнинг

--- src/helpers/sentry.js
+++ src/helpers/sentry.js
@@ -5,7 +5,7 @@ export const initSentry = () => {
         console.warn(new Error('Sentry wouldn’t run at localhost'))
     }
 
-    window.Sentry?.init({
+    window.Sentry.init({
         dsn: 'https://30b586ae715c4e19b6654cb98b5412e8@sentry.io/2958514'
     });
 };
