231333bc1bec7cd82ee6a1065d51c8182d4dfeb6
Vladimir Pechischev <pechischev.vladimir@gmail.com>
2020-02-28 19:45:04 +0300

    feature(#13): отправлены файлы для sentry

---
+++ src/helpers/sentry.js
@@ -0,0 +1,11 @@
+const isDev = process && process.env && process.env.NODE_ENV === 'development';
+
+export const initSentry = () => {
+    if (isDev) {
+        console.warn(new Error('Sentry wouldn’t run at localhost'))
+    }
+
+    window.Sentry?.init({
+        dsn: 'https://30b586ae715c4e19b6654cb98b5412e8@sentry.io/2958514'
+    });
+};
