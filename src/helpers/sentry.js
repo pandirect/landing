6dea4cd2b8a0942166989abb9dbbfd4452af11e0
Vladimir Pechischev <pechischev.vladimir@gmail.com>
2020-02-28 20:43:59 +0300

    fix: поправлена сборка

--- src/helpers/sentry.js
+++ src/helpers/sentry.js
@@ -1,11 +1,5 @@
-const isDev = process && process.env && process.env.NODE_ENV === 'development';
-
 export const initSentry = () => {
-    if (isDev) {
-        console.warn(new Error('Sentry wouldn’t run at localhost'))
-    }
-
-    window.Sentry.init({
-        dsn: 'https://30b586ae715c4e19b6654cb98b5412e8@sentry.io/2958514'
-    });
+  window.Sentry.init({
+    dsn: 'https://30b586ae715c4e19b6654cb98b5412e8@sentry.io/2958514'
+  });
 };
